e7e6348764b22fa302bc5dc22e6a664261aa28ef
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2025-05-29 23:49:54 +0800

    [Feature][Shua] Added banner and skeleton ui

---
+++ src/components/Banner.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-[url(/banner.jpg)] bg-cover bg-center h-64 flex justify-center align-middle">
+    <span class="h-fit m-auto">
+      Psalmantics
+    </span>
+  </div>
+</template>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from '../components/HelloWorld.vue'
+import Banner from '../components/Banner.vue'
+
+import { ref } from 'vue'
+
+const query = ref('')
+
+function searchQuery() {
+  console.log(query.value)
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Banner />
+  <input type="text" v-model="query">
+  <button @click="searchQuery">
+    Search
+  </button>
 </template>
 
 <style scoped>
