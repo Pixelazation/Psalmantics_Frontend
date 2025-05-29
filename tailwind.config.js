6f0a3767e2178df81b899b7e984a91ba643d5ee1
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2025-05-29 22:09:55 +0800

    [Setup][Shua] Added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#CC9900",
+        red: "#7E0021",
+        green: "#314004"
+      },
+      fontFamily: {
+        bookman: ['"Libre Baskerville"', 'Georgia', 'serif'],
+      },
+    },
+    fontFamily: {
+      Roboto: "Roboto",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+  },
+  plugins: [],
+};
